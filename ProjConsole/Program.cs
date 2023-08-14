2ad7c4baef4e5e42b754524a28da142fc2b8dd57
nabruna <brunanegrellideandrade@gmail.com>
2023-08-14 20:54:42 -0300

    console files

---
+++ ProjConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.Contracts;
+using System.Security.Cryptography.X509Certificates;
+
+int size = 20;
+int[] arrUnord = new int[size];
+Random random = new();
+
+for (int i = 0; i < size; i++)
+{
+    arrUnord[i] = random.Next(1, 1000);
+}
+
+PrintArr();
+
+void PrintArr() {
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(arrUnord[i] + " ");
+    }
+    Console.WriteLine("\n");
+}
+
+Array.Sort(arrUnord);
+
+PrintArr();
